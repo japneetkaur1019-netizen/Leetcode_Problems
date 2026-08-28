413398b34a1a0a9fe2540ee04c19836d390e6a87
japneetkaur1019-netizen <japneetkaur1019@gmail.com>
2026-08-28 23:00:02 +0530

    Time: 3 ms (80.74%), Space: 43.1 MB (96.34%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+                stack.pop();
+            }
+
+            else if (op.equals("D")) {
+                int last = stack.peek();
+                stack.push(last * 2);
+            }
+
+            else if (op.equals("+")) {
+                int last = stack.pop();
+                int secondLast = stack.peek();
+
+                int sum = last + secondLast;
+
+                stack.push(last);
+                stack.push(sum);
+            }
+
+            else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        return sum;
+    }
+}
