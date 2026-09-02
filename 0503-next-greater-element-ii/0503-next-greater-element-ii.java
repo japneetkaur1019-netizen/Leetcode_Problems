22f45bf9b4348fcd34c486407e4a8fb5ca027a1d
japneetkaur1019-netizen <japneetkaur1019@gmail.com>
2026-09-02 20:56:28 +0530

    Time: 16 ms (34.55%), Space: 48.9 MB (6.67%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int index = i % n;
+
+           while (!stack.isEmpty() &&
+                   stack.peek() <= nums[index]) {
+                stack.pop();
+            }
+            if (i < n) {
+                if (stack.isEmpty()) {
+                    result[index] = -1;
+                } else {
+                    result[index] = stack.peek();
+                }
+            }
+            stack.push(nums[index]);
+        }
+
+        return result;
+    }
+}
