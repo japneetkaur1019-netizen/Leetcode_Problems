c3d6f1433e54fd50b70e698a4e7ab55390b92974
japneetkaur1019-netizen <japneetkaur1019@gmail.com>
2026-09-16 00:37:43 +0530

    Time: 0 ms (100%), Space: 42 MB (58.3%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public int nextGreaterElement(int n) {
+
+        char[] arr = String.valueOf(n).toCharArray();
+
+        int i = arr.length - 2;
+
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+
+        if (i < 0) {
+            return -1;
+        }
+
+        int j = arr.length - 1;
+
+        while (arr[j] <= arr[i]) {
+            j--;
+        }
+
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        reverse(arr, i + 1, arr.length - 1);
+
+        long result = Long.parseLong(new String(arr));
+
+        if (result > Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        return (int) result;
+    }
+
+    private void reverse(char[] arr, int left, int right) {
+
+        while (left < right) {
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
